9791324cfda1195393de8eec9eea67aa98386f7d
SpamixOfficial <13174934-SpamixOfficial@users.noreply.gitlab.com>
2023-12-18 21:32:15 +0100

    Continuing tomorrow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
+use std::env;
+use std::process::exit;
+use uname::uname;
+
+struct OsInfo {
+    os_type: String,
+    os_arch: String,
+    shell: String,
+    username: String,
+    os_release: String,
+    hostname: String,
+}
+
+impl os_info {
+    fn new() -> Self {
+        let os_type = env::consts::OS;
+
+        let os_arch = env::consts::ARCH;
+        let shell = match env::var_os("SHELL") {
+            Some(v) => v.into_string().unwrap(),
+            None => String::from("Unknown")
+        };
+        let username = match env::var_os("LOGNAME") {
+            Some(v) => v.into_string().unwrap(),
+            None => String::from("unknown")
+        };
+        let os_release = uname().unwrap().release;
+        let hostname = uname().unwrap().nodename; 
+    }
+}
+
+fn get_os_info() -> [String; 6] {
+    let os_type = env::consts::OS;
+
+    if os_type == "windows" {
+        println!("Windows is not supported at the moment.");
+        exit(1);
+    };
+    let os_arch = env::consts::ARCH;
+    let shell = match env::var_os("SHELL") {
+        Some(v) => v.into_string().unwrap(),
+        None => String::from("Unknown")
+    };
+    let username = match env::var_os("LOGNAME") {
+        Some(v) => v.into_string().unwrap(),
+        None => String::from("none")
+    };
+    let os_release = uname().unwrap().release;
+    let hostname = uname().unwrap().nodename;
+    return [os_type.to_string(), os_arch.to_string(), shell, username, os_release, hostname]
+}
+
+fn create_out()
+
 fn main() {
-    println!("Hello, world!");
+    let info = get_os_info();
+    
 }
