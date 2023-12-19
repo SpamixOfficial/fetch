5cfd1c3c0deb8b6848d156305ec21296d49be6b4
SpamixOfficial <13174934-SpamixOfficial@users.noreply.gitlab.com>
2023-12-19 07:42:06 +0000

    fixed

--- target/debug/deps/fetch-45e861926e21b91f.d
+++
@@ -1,5 +0,0 @@
-/home/alexander/Documents/GitHub/fetch/target/debug/deps/fetch-45e861926e21b91f.rmeta: src/main.rs
-
-/home/alexander/Documents/GitHub/fetch/target/debug/deps/fetch-45e861926e21b91f.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/fetch-a5ac7c31fa090d84.d
+++
@@ -1,5 +0,0 @@
-/home/alexander/Documents/GitHub/fetch/target/debug/deps/fetch-a5ac7c31fa090d84.rmeta: src/main.rs
-
-/home/alexander/Documents/GitHub/fetch/target/debug/deps/fetch-a5ac7c31fa090d84.d: src/main.rs
-
-src/main.rs:
