d68210596b899a9a655a00daa4515c68b14381cd
SpamixOfficial <99183771+SpamixOfficial@users.noreply.github.com>
2023-12-19 16:06:32 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::env;
-use std::process::exit;
 use uname::uname;
 
 struct OsInfo {
@@ -11,48 +10,42 @@ struct OsInfo {
     hostname: String,
 }
 
-impl os_info {
+impl OsInfo {
     fn new() -> Self {
-        os_type = env::consts::OS;
+        Self {
+            os_type: env::consts::OS.to_string(),
 
-        os_arch = env::consts::ARCH;
-        shell = match env::var_os("SHELL") {
-            Some(v) => v.into_string().unwrap(),
-            None => String::from("Unknown")
-        };
-        username = match env::var_os("LOGNAME") {
-            Some(v) => v.into_string().unwrap(),
-            None => String::from("unknown")
-        };
-        os_release = uname().unwrap().release;
-        hostname = uname().unwrap().nodename; 
+            os_arch: env::consts::ARCH.to_string(),
+            shell: match env::var_os("SHELL") {
+                Some(v) => v.into_string().unwrap(),
+                None => String::from("Unknown"),
+            },
+            username: match env::var_os("LOGNAME") {
+                Some(v) => v.into_string().unwrap(),
+                None => String::from("unknown"),
+            },
+            os_release: uname().unwrap().release,
+            hostname: uname().unwrap().nodename,
+        }
     }
 }
 
-fn get_os_info() -> [String; 6] {
-    let os_type = env::consts::OS;
-
-    if os_type == "windows" {
-        println!("Windows is not supported at the moment.");
-        exit(1);
-    };
-    let os_arch = env::consts::ARCH;
-    let shell = match env::var_os("SHELL") {
-        Some(v) => v.into_string().unwrap(),
-        None => String::from("Unknown")
-    };
-    let username = match env::var_os("LOGNAME") {
-        Some(v) => v.into_string().unwrap(),
-        None => String::from("none")
-    };
-    let os_release = uname().unwrap().release;
-    let hostname = uname().unwrap().nodename;
-    return [os_type.to_string(), os_arch.to_string(), shell, username, os_release, hostname]
+fn get_ascii() {
+    println!("Placeholder");    
 }
 
-
+fn create_output(info: OsInfo) -> String {
+    let mut outstr = String::new();
+     
+    outstr.push_str(format!("* {}@{}\n", info.username, info.hostname).as_str());
+    outstr.push_str(format!("* OS:\t\t{}\n", info.os_type).as_str());
+    outstr.push_str(format!("* Kernel:\t{}\n", info.os_release).as_str());
+    outstr.push_str(format!("* ").as_str());
+    return outstr;
+}
 
 fn main() {
-    let info = ;
-    
+    let info = OsInfo::new();
+    let output = create_output(info);
+    println!("{}", output);
 }
