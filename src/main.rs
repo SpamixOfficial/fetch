c7630cd0e92a0d645225c5989b1f8df0055f7a81
SpamixOfficial <13174934-SpamixOfficial@users.noreply.gitlab.com>
2023-12-26 11:06:18 +0100

    Got ascii art working, adding more standard art and features now

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-use std::{
-    env,
-    fs,
-    path
-};
+use std::{env, fs, path};
 use uname::uname;
 
 struct OsInfo {
@@ -18,7 +14,6 @@ impl OsInfo {
     fn new() -> Self {
         Self {
             os_type: env::consts::OS.to_string(),
-
             os_arch: env::consts::ARCH.to_string(),
             shell: match env::var_os("SHELL") {
                 Some(v) => v.into_string().unwrap(),
@@ -57,19 +52,26 @@ __
         };
     } else {
         art = std_unknown_art.to_string();
-    }; 
+    };
     art
 }
 
 fn create_output(art: String, info: OsInfo) -> String {
+    // initialize the output string
     let mut outstr = String::new();
 
+    // initialize temporary "fields strings"
+    let mut tmp_fieldstrings: Vec<String> = vec![];
+
+    // get all art lines
     let art_lines: Vec<&str> = art.split("\n").filter(|&x| !x.is_empty()).collect();
     if art_lines.len() > 8 {
         eprintln!("Error, height of ascii art is more than 8...");
         std::process::exit(1);
     };
+    // get the longest art line
     let longest_art_line = &art_lines.iter().max().unwrap().len();
+    // get all the fields
     let user_host = format!("{}@{}", info.username, info.hostname);
     let os = &info.os_type;
     let arch = &info.os_arch;
@@ -77,83 +79,72 @@ fn create_output(art: String, info: OsInfo) -> String {
     let shell = &info.shell;
 
     let params = [&user_host, &os, &arch, &kernel, &shell];
+    // get longest param (will redo)
     let mut lastlength = 0;
     for param in params {
-        let length = param.len(); 
+        let length = param.len();
         if length > lastlength {
             lastlength = length;
         };
-    }; 
-    let height = 8;
-    let wait = height/2 - ((art_lines.len() as f32 /2.0).ceil() as usize); 
-    let mut wait_counter = wait.clone();
-    //debug
-    println!("{}", wait);
+    }
+    
     let param_names = ["", "OS", "Arch", "Kernel", "Shell"];
-    let mut i = 0;
-   
-    while i < param_names.len() {
-        let mut tempstr = String::new();
-        let mut ascii_str = String::new();
-        let mut spaces_needed = longest_art_line.to_owned();
-        if wait_counter > 0 {
-            ascii_str.push_str(format!("{:>longest_art_line$}", "").as_str());
+    // Add whole field to vector
+    for i in 0..param_names.len() {
+        if i == 0 {
+            tmp_fieldstrings.push(format!("┏{:━>lastlength$}┓", ""));
+        }
+        if param_names[i] != "" {
+            let numspaces = &lastlength - &param_names[i].len() - 3;
+            tmp_fieldstrings.push(format!("┃ {}:{:>numspaces$} ┃", param_names[i], params[i]));
+        } else {
+            tmp_fieldstrings.push(format!("┃{}┃", params[i]));
+            if i == 0 {
+                tmp_fieldstrings.push(format!("┣{:━>lastlength$}┫", ""))
+            }
+        };
+        if i == param_names.len() - 1 {
+            tmp_fieldstrings.push(format!("┗{:━>lastlength$}┛", ""));
+        }
+    }
+    let height = tmp_fieldstrings.len();
+    let out_length = if height > art_lines.len() {
+        height
+    } else {
+        art_lines.len()
+    };
+    let wait = if height > art_lines.len() {
+        ((height as f32 / 2.0).floor() - (art_lines.len() as f32 / 2.0).floor()) as usize
+    } else {
+        ((art_lines.len() as f32 / 2.0).floor() - (height as f32 / 2.0).floor()) as usize
+    };
+    let mut wait_counter = wait.clone();
+    for i in 0..out_length {
+        let spaces_needed: usize;
+        let artline = if wait_counter == 0 && i-wait < art_lines.len() {
+            spaces_needed = longest_art_line - art_lines[i - wait].len();
+            art_lines[i - wait].to_string()
+        } else {
+            spaces_needed = longest_art_line.to_owned();
+            String::from("")
+        };
+        if wait_counter != 0 {
             wait_counter -= 1;
-        } else if art_lines.len() < i-wait {
-            ascii_str.push_str(format!("{:>longest_art_line$}", "").as_str());
-        } else { 
-            ascii_str = art_lines[i+1-wait].to_string();
-            spaces_needed = longest_art_line-art_lines[i].len();
-        };  
+        };
+        let fieldstring = if i < tmp_fieldstrings.len() {
+            tmp_fieldstrings[i].as_str().to_string()
+        } else {
+            String::from("")
+        }; 
+        outstr.push_str(format!("  {}{:>spaces_needed$}  {}\n", artline, "", fieldstring).as_str());
+    }
 
-        tempstr.push_str("  ");
-        tempstr.push_str(ascii_str.as_str());
-        tempstr.push_str(format!("{:>spaces_needed$}  ", " ").as_str()); 
-        if i == 0 {
-            tempstr.push_str(format!("┏{:━>lastlength$}┓\n", "").as_str());
-        }
-        if param_names[i] != "" {
-            let numspaces = &lastlength - &param_names[i].len() - 3; 
-            tempstr.push_str(format!("┃ {}:{:>numspaces$} ┃\n", param_names[i], params[i]).as_str());
-        } else { 
-            tempstr.push_str(format!("┃{}┃\n", params[i]).as_str());
-            if i == 0 {
-                tempstr.push_str(format!("┣{:━>lastlength$}┫\n", "").as_str())
-            }
-        };
-        if i == param_names.len()-1 {
-            tempstr.push_str(format!("┗{:━>lastlength$}┛\n", "").as_str());
-        };
-        dbg!(&tempstr);
-        outstr.push_str(tempstr.as_str());
-        i += 1;
-    }; 
-/*
-    while i < param_names.len() {
-        if i == 0 {
-            outstr.push_str(format!("┏{:━>lastlength$}┓\n", "").as_str());
-        }
-        if param_names[i] != "" {
-            let numspaces = &lastlength - &param_names[i].len() - 3; 
-            outstr.push_str(format!("┃ {}:{:>numspaces$} ┃\n", param_names[i], params[i]).as_str());
-        } else { 
-            outstr.push_str(format!("┃{}┃\n", params[i]).as_str());
-            if i == 0 {
-                outstr.push_str(format!("┣{:━>lastlength$}┫\n", "").as_str())
-            }
-        };
-        if i == param_names.len()-1 {
-            outstr.push_str(format!("┗{:━>lastlength$}┛\n", "").as_str());
-        }
-        i += 1;
-    }; */
     outstr
 }
 
 fn main() {
     let info = OsInfo::new();
     let art = get_ascii(&info);
-    println!("{}", art);
-    let output = create_output(art, info); 
+    let output = create_output(art, info);
     println!("{}", output);
 }
